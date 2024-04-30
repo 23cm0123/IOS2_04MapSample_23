6ba5b1e899f6b6e221c88b550202f85e2f7d16a7
Kristen23 <137244397+Kristen-w-Yui@users.noreply.github.com>
2024-04-30 13:42:52 +0900

    Initial Commit

---
+++ IOS2_04MapSample_23/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IOS2_04MapSample_23
+//
+//  Created by Kristen on 2024/04/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IOS2_04MapSample_23/IOS2_04MapSample_23App.swift
@@ -0,0 +1,17 @@
+//
+//  IOS2_04MapSample_23App.swift
+//  IOS2_04MapSample_23
+//
+//  Created by Kristen on 2024/04/30.
+//
+
+import SwiftUI
+
+@main
+struct IOS2_04MapSample_23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
